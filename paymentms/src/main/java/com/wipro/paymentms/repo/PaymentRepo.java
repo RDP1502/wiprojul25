5fba081a7f985992b7426c722aac4f96efcc085e
RDP1502 <rdpawar@gmail.com>
2025-08-06 08:45:49 +0530

    made public and added new files

---
+++ paymentms/src/main/java/com/wipro/paymentms/repo/PaymentRepo.java
@@ -0,0 +1,10 @@
+package com.wipro.paymentms.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wipro.paymentms.entity.Payment;
+
+public interface PaymentRepo extends JpaRepository<Payment, Integer> {
+	 Payment findByOrderId(int orderId);
+
+}
